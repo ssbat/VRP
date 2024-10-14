bba9260b93eb99a3000cf284956a3f11c2654230
Boby974 <b.cuenot@orange.fr>
2024-10-14 12:24:19 +0200

    feat : add croisement function

--- CVRPTW.py
+++ CVRPTW.py
@@ -1,5 +1,7 @@
 from CVRPTW_info import CVRPTWInfo
 from CVRPTW_population import Population
+from CVRPTW_chromosome import Chromosome
+import random
 
 
 class CVRPTW:
@@ -13,13 +15,47 @@ class CVRPTW:
         pass
 
 
-    def croisement(self,chromsomes):
-        pass
+    def croisement(self, chromosomes : list) -> list[Chromosome]:
+        children = []
+        while chromosomes:
+            parent_1, parent_2 = random.sample(chromosomes, 2)
+            chromosomes.remove(parent_1)
+            chromosomes.remove(parent_2)
+
+            size = len(parent_1.chromosome)
+            start_index_crossing_point  = size // 3
+            end_index_crossing_point = start_index_crossing_point * 2
+
+            child_1 = [None] * len(parent_1.chromosome)
+            child_1[start_index_crossing_point:end_index_crossing_point] = parent_1.chromosome[start_index_crossing_point:end_index_crossing_point]
+
+            index_child = 0
+            for city in parent_2.chromosome:
+                if city not in child_1:
+                    while child_1[index_child] is not None:
+                        index_child += 1
+                    child_1[index_child] = city
+
+            child_2 = [None] * len(parent_2.chromosome)
+            child_2[start_index_crossing_point:end_index_crossing_point] = parent_2.chromosome[start_index_crossing_point:end_index_crossing_point]
+ 
+            index_child = 0
+            for city in parent_1.chromosome:
+                if city not in child_2:
+                    while child_2[index_child] is not None:
+                        index_child += 1
+                    child_2[index_child] = city
+
+            children.append(Chromosome(self.info, child_1))
+            children.append(Chromosome(self.info, child_2))
+
+        return children
 
     def optimize(self):
         nb_enfant= 4
         for generation in range(self.nb_generation) :
             parents=self.population.rank_selection_sorted(nb_enfant)
+            self.croisement(parents)
             pass
         pass
     
