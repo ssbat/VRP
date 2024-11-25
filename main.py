2632ff0afcd89240b4c63c7d9b372e64681fcfd9
Boby974 <b.cuenot@orange.fr>
2024-11-25 17:29:12 +0100

    feat : add csv writing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from CVRPTW import CVRPTW
 from CVRPTW_info import CVRPTWInfo
 from Tabou import Tabou
 from CVRPTW_params import *
+from Results.ResultsManager import ResultsManager
 
 info = CVRPTWInfo(FULL_INSTANCE_NAME,CLIENTS_NUMBER)
 AG = CVRPTW(info)
@@ -10,3 +11,7 @@ AG.optimize()
 
 TABOU = Tabou(AG.population.best_solution)
 TABOU.optimize()
+
+csv_path = 'Results/params.csv'
+RESULTS_MANAGER = ResultsManager(AG.population.best_solution, TABOU.best_chromosome, csv_path)
+RESULTS_MANAGER.save_results_to_csv()
