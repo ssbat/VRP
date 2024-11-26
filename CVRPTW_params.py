557debcd2661588b9ab75b736eb38081b753fb6e
joelkdb <43908315+joelkdb@users.noreply.github.com>
2024-11-26 15:45:25 +0100

    Tabou research params retrieve bug fix.

--- CVRPTW_params.py
+++ CVRPTW_params.py
@@ -7,7 +7,18 @@ from utils import *
 # FULL_INSTANCE_NAME = f'instances/{INSTANCE_NAME}.{CLIENTS_NUMBER}.txt'
 # NB_ITERATIONS = 5000
 # POPULATION_SIZE = 300
+# INSTANCE_NAME= 'R101'
+# CLIENTS_NUMBER = ClientNumber.TwentyFive.value
+# FULL_INSTANCE_NAME = f'instances/{INSTANCE_NAME}.{CLIENTS_NUMBER}.txt'
+# NB_ITERATIONS = 5000
+# POPULATION_SIZE = 300
 
+# WAIT_COEFF = 0.5
+# DELAY_COEFF = 1.6
+# NB_VEHICULES_COEFF = 50
+
+# CX_PROBA = 0.85
+# MUT_PROBA = 0.5
 # WAIT_COEFF = 0.5
 # DELAY_COEFF = 1.6
 # NB_VEHICULES_COEFF = 50
@@ -17,10 +28,12 @@ from utils import *
 
 # Constantes pour la méthode tabou
 
-# TABOU_SEARCH_ON = True
-TABOU_LIST_SIZE_MAX = 10
+TABOU_SEARCH_ON = True
+# TABOU_LIST_SIZE_MAX = 10
 # TABOU_NEIGHBOURHOOD_SIZE = 200
 # TABOU_NB_ITERATIONS = 3000
+
+CSV_PATH = 'Results/params.csv'
 
 
 # !! Load params from JSON file
@@ -28,34 +41,6 @@ parameters = load_parameters_from_file()
 
 INSTANCE_NAME = parameters.get("INSTANCE_NAME", "R101")
 CLIENTS_NUMBER = parameters.get("CLIENTS_NUMBER", ClientNumber.TwentyFive.value)
-FULL_INSTANCE_NAME = f"instances/{INSTANCE_NAME}.{CLIENTS_NUMBER}.txt"
-
-NB_ITERATIONS = parameters.get("NB_ITERATIONS", 5000)
-POPULATION_SIZE = parameters.get("POPULATION_SIZE", 300)
-
-WAIT_COEFF = parameters.get("WAIT_COEFF", 0.5)
-DELAY_COEFF = parameters.get("DELAY_COEFF", 1.6)
-NB_VEHICULES_COEFF = parameters.get("NB_VEHICULES_COEFF", 50)
-
-CX_PROBA = parameters.get("CX_PROBA", 0.85)
-MUT_PROBA = parameters.get("MUT_PROBA", 0.5)
-
-# Tabou research parameters
-TABOU_LIST_SIZE_MAX = parameters.get("TABOU_LIST_SIZE_MAX", 10)
-TABOU_NEIGHBOURHOOD_SIZE = parameters.get("TABOU_NEIGHBOURHOOD_SIZE", 200)
-TABOU_NB_ITERATIONS = parameters.get("TABOU_NB_ITERATIONS", 3000)
-
-# Selected methode
-METHOD = parameters.get("METHOD", "Algorithme Génétique")
-
-CSV_PATH = 'Results/params.csv'
-
-
-# load params from JSON file
-parameters = load_parameters_from_file()
-
-INSTANCE_NAME = parameters.get("INSTANCE_NAME", "R101")
-CLIENTS_NUMBER = parameters.get("CLIENTS_NUMBER", 25)
 FULL_INSTANCE_NAME = f"instances/{INSTANCE_NAME}.{CLIENTS_NUMBER}.txt"
 
 NB_ITERATIONS = parameters.get("NB_ITERATIONS", 5000)
